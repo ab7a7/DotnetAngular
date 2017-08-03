3fc641369da3dcce139e3f0d43a439a9872426c9
Abrar Ali <ali20011@hotmail.com>
2017-08-03 22:16:37 +0100

    Initial Commit

---
+++ src/Web.Ng/ng-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   title = "app";
+    apiValues: string[];
+
+    constructor(private httpService: Http) { }
+
+    ngOnInit() {
+        this.httpService.get("api/values")
+            .subscribe(res => this.apiValues = res.json() as string[]);
+    }
+}
